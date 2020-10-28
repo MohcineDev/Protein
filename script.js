2ef273a71f95f0411cd60c9418201061b8426d1e
mohcineDev <mouhcinedev@gmail.com>
2020-10-28 12:29:26 +0100

    add files

---
+++ script.js
@@ -0,0 +1,47 @@
+const scrollbtn = document.querySelector('#scroll')
+const scrollTop = document.querySelector('#scroll-top')
+const mail = document.querySelectorAll('#mail')
+
+
+const height = innerHeight
+
+scrollbtn.addEventListener('click', () => {
+
+    scrollBy({
+        top: height,
+        behavior: "smooth"
+    })
+    scrollbtn.style.display = 'none'
+})
+
+document.addEventListener('scroll', () => {
+
+    if (scrollY >= height * 2) {
+        scrollTop.style.display = 'block'
+
+    }
+    else
+        scrollTop.style.display = 'none'
+})
+
+scrollTop.addEventListener('click', () => {
+
+    scroll({
+        top: 0,
+        behavior: "smooth"
+    })
+
+})
+
+
+// hide the placeholder when mouse click - enter - focus
+mail.forEach(item => {
+
+    item.addEventListener('focus', () => {
+        item.setAttribute('placeholder', '')
+    })
+
+    item.addEventListener('blur', () => {
+        item.setAttribute('placeholder', 'Your Email ...')
+    })
+});
