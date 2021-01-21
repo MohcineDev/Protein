049e15c22553b54f2896d6299e273747462a042c
MohcineDev <mouhcinedev@gmail.com>
2021-01-21 22:12:54 +0000

    add default js

---
+++ main.js
@@ -0,0 +1,6 @@
+
+const display = document.querySelector('#display')
+const hide = document.querySelector('#hide')
+const YEAR = document.querySelector('#year')
+
+YEAR.textContent = new Date().getFullYear()
