4b8065a2fe71e5eb259052247e080f824b0af1f4
MohcineDev <Mouhcinedev@gmail.com>
2021-01-23 12:42:17 +0000

    some updates

--- main.js
+++ main.js
@@ -2,5 +2,48 @@
 const display = document.querySelector('#display')
 const hide = document.querySelector('#hide')
 const YEAR = document.querySelector('#year')
+const links = document.querySelector('.links')
+
+display.addEventListener('click', function () {
+    this.style.display = 'none'
+    hide.style.display = 'block'
+    links.style.right = '0px'
+    this.style.animation = 'rotate .5s ease-out';
+    hide.style.animation = 'rotate .5s ease-out';
+})
+
+const hideBtn = () => {
+
+    if (this.innerWidth <= 768) {
+        hide.style.display = 'none'
+        display.style.display = 'block'
+        links.style.right = '-100%'
+    }
+}
+
+//hide nav bar
+hide.addEventListener('click', function () {
+    hideBtn()
+})
+
+//hide the nav when an item is clicked
+for (let i = 0; i < links.children.length; i++) {
+
+    links.children[i].addEventListener('click', () => {
+        hideBtn()
+    })
+}
+
+window.addEventListener('resize', () => {
+    if (this.innerWidth > 768) {
+        display.style.display = 'none'
+        hide.style.display = 'none'
+    }
+    else {
+        display.style.display = 'block'
+        hide.style.display = 'none'
+        links.style.right = '-100%'
+    }
+})
 
 YEAR.textContent = new Date().getFullYear()
--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const scrollDown = document.querySelector('#scroll')
 const scrollTop = document.querySelector('#scroll-top')
 const mail = document.querySelectorAll('#mail')
-
-const links = document.querySelector('.links')
-
 const height = innerHeight
 
 scrollDown.addEventListener('click', () => {
@@ -33,7 +30,6 @@ scrollTop.addEventListener('click', () => {
 
 })
 
-
 // hide the placeholder when mouse click - enter - focus
 mail.forEach(item => {
 
@@ -45,51 +41,6 @@ mail.forEach(item => {
         item.setAttribute('placeholder', 'Your Email ...')
     })
 });
+ 
 
-btn.addEventListener('click', () => {
-    console.log(123);
-})
-
-display.addEventListener('click', function () {
-    this.style.display = 'none'
-    hide.style.display = 'block'
-    links.style.right = '0px'
-    this.style.animation = 'rotate .5s ease-out';
-    hide.style.animation = 'rotate .5s ease-out';
-})
-
-const hideBtn = () => {
-
-    if (this.innerWidth <= 768) {
-        hide.style.display = 'none'
-        display.style.display = 'block'
-        links.style.right = '-100%'
-    }
-}
-
-hide.addEventListener('click', function () {
-    hideBtn()
-})
-
-//hide the nav when an item is clicked
-for (let i = 0; i < links.children.length; i++) {
-
-    links.children[i].addEventListener('click', () => {
-        hideBtn()
-    })
-}
-
-
-window.addEventListener('resize', () => {
-    if (this.innerWidth > 768) {
-        display.style.display = 'none'
-        hide.style.display = 'none'
-    }
-    else {
-        display.style.display = 'block'
-        hide.style.display = 'none'
-        links.style.right = '-100%'
-    }
-
-})
 
